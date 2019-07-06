fc8925562959dbf73d92538e1b4e2882abd8d066
bnesoi <bnesoi@student.21-school.ru>
2019-07-06 14:35:59 +0300

    mat4_transform.c - norm

--- src/mat4_transform.c
+++ src/mat4_transform.c
@@ -1,41 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mat4_transform.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bnesoi <bnesoi@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/06 14:33:58 by bnesoi            #+#    #+#             */
+/*   Updated: 2019/07/06 14:36:56 by bnesoi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "fdf.h"
 
-void			m4_translate(t_mat4 *m, t_vec3 *t)
+void	m4_translate(t_mat4 *m, t_vec3 *t)
 {
-	m->r[3][0] += t->v[0] * m->r[0][0] + t->v[1] * m->r[1][0] + t->v[2] * m->r[2][0];
-	m->r[3][1] += t->v[0] * m->r[0][1] + t->v[1] * m->r[1][1] + t->v[2] * m->r[2][1];
-	m->r[3][2] += t->v[0] * m->r[0][2] + t->v[1] * m->r[1][2] + t->v[2] * m->r[2][2];
-	m->r[3][3] += t->v[0] * m->r[0][3] + t->v[1] * m->r[1][3] + t->v[2] * m->r[2][3];
+	m->r[3][0] +=
+			t->v[0] * m->r[0][0] +
+			t->v[1] * m->r[1][0] +
+			t->v[2] * m->r[2][0];
+	m->r[3][1] +=
+			t->v[0] * m->r[0][1] +
+			t->v[1] * m->r[1][1] +
+			t->v[2] * m->r[2][1];
+	m->r[3][2] +=
+			t->v[0] * m->r[0][2] +
+			t->v[1] * m->r[1][2] +
+			t->v[2] * m->r[2][2];
+	m->r[3][3] +=
+			t->v[0] * m->r[0][3] +
+			t->v[1] * m->r[1][3] +
+			t->v[2] * m->r[2][3];
 }
 
-void			m4_setRotation(t_mat4 *m, t_vec3 *r)
+void	m4_set_rotation(t_mat4 *m, t_vec3 *r)
 {
-	t_real cos_rz;
-	t_real cos_ry;
-	t_real cos_rx;
-	t_real sin_rz;
-	t_real sin_ry;
-	t_real sin_rx;
+	t_real	cos_v[3];
+	t_real	sin_v[3];
 
 	m4_identity(m);
-	cos_rz = cos(TO_RAD(r->v[2]));
-	cos_ry = cos(TO_RAD(r->v[1]));
-	cos_rx = cos(TO_RAD(r->v[0]));
-	sin_rz = sin(TO_RAD(r->v[2]));
-	sin_ry = sin(TO_RAD(r->v[1]));
-	sin_rx = sin(TO_RAD(r->v[0]));
-	m->r[0][0] = cos_rz * cos_ry;
-	m->r[0][1] = sin_rz * cos_ry;
-	m->r[0][2] = -sin_ry;
-	m->r[1][0] = -sin_rz * cos_rx + cos_rz * sin_ry * sin_rx;
-	m->r[1][1] = cos_rz * cos_rx + sin_rz * sin_ry * sin_rx;
-	m->r[1][2] = cos_ry * sin_rx;
-	m->r[2][0] = -sin_rz * -sin_rx + cos_rz * sin_ry * cos_rx;
-	m->r[2][1] = cos_rz * -sin_rx + sin_rz * sin_ry * cos_rx;
-	m->r[2][2] = cos_ry * cos_rx;
+	cos_v[2] = cos(TO_RAD(r->v[2]));
+	cos_v[1] = cos(TO_RAD(r->v[1]));
+	cos_v[0] = cos(TO_RAD(r->v[0]));
+	sin_v[2] = sin(TO_RAD(r->v[2]));
+	sin_v[1] = sin(TO_RAD(r->v[1]));
+	sin_v[0] = sin(TO_RAD(r->v[0]));
+	m->r[0][0] = cos_v[2] * cos_v[1];
+	m->r[0][1] = sin_v[2] * cos_v[1];
+	m->r[0][2] = -sin_v[1];
+	m->r[1][0] = -sin_v[2] * cos_v[0] + cos_v[2] * sin_v[1] * sin_v[0];
+	m->r[1][1] = cos_v[2] * cos_v[0] + sin_v[2] * sin_v[1] * sin_v[0];
+	m->r[1][2] = cos_v[1] * sin_v[0];
+	m->r[2][0] = -sin_v[2] * -sin_v[0] + cos_v[2] * sin_v[1] * cos_v[0];
+	m->r[2][1] = cos_v[2] * -sin_v[0] + sin_v[2] * sin_v[1] * cos_v[0];
+	m->r[2][2] = cos_v[1] * cos_v[0];
 }
 
-void			m4_rotate(t_mat4 *m, t_vec3 *r)
+void	m4_rotate(t_mat4 *m, t_vec3 *r)
 {
 	t_mat4	mr;
 	t_mat4	mc;
@@ -44,8 +64,7 @@ void			m4_rotate(t_mat4 *m, t_vec3 *r)
 	int		k;
 
 	mc = *m4_copy(m, &mc);
-	m4_setRotation(&mr, r);
-
+	m4_set_rotation(&mr, r);
 	rr = 3;
 	while (rr--)
 	{
@@ -61,16 +80,16 @@ void			m4_rotate(t_mat4 *m, t_vec3 *r)
 	}
 }
 
-void			m4_scale(t_mat4 *m, t_vec3 *s)
+void	m4_scale(t_mat4 *m, t_vec3 *s)
 {
 	int r;
 	int c;
 
 	r = 3;
-	while(r--)
+	while (r--)
 	{
 		c = 3;
-		while(c--)
+		while (c--)
 			m->r[r][c] *= s->v[r];
 	}
 }
