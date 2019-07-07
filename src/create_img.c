fe506f1aada3c3216a80ad24b9aab0ec95d7f83c
Shanna Sheba <ssheba@oa-i2.21-school.ru>
2019-07-07 15:16:37 +0300

    norm

--- src/create_img.c
+++ src/create_img.c
@@ -6,7 +6,7 @@
 /*   By: ssheba <ssheba@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/17 07:36:27 by ssheba            #+#    #+#             */
-/*   Updated: 2019/07/07 15:10:54 by ssheba           ###   ########.fr       */
+/*   Updated: 2019/07/07 15:15:54 by ssheba           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,13 @@
 static void	set_pnt_to_img(t_point *pos, t_img *img)
 {
 	printf("set_pnt: (%f, %f, %f)\n", pos->c.x, pos->c.y, pos->c.z);
-	if (pos->c.x + pos->c.z * cos(ANGLE) < img->size_x && pos->c.y + pos->c.z * sin(ANGLE) < img->size_y)
-		img->pic[(size_t)(img->size_y * (int)(pos->c.x + pos->c.z * cos(ANGLE)) + (int)(pos->c.y + pos->c.z * sin(ANGLE)))] = 0;
+	if (pos->c.x + pos->c.z * cos(ANGLE) < img->size_x && pos->c.y + \
+	pos->c.z * sin(ANGLE) < img->size_y)
+		img->pic[(size_t)(img->size_y * (int)(pos->c.x + pos->c.z * \
+		cos(ANGLE)) + (int)(pos->c.y + pos->c.z * sin(ANGLE)))] = 0;
 }
 
-static void set_line_to_img(t_point *a, t_point *b, t_img *img)
+static void	set_line_to_img(t_point *a, t_point *b, t_img *img)
 {
 	t_point	c;
 	double	dx;
@@ -39,7 +41,8 @@ static void set_line_to_img(t_point *a, t_point *b, t_img *img)
 	c.c.y = a->c.y;
 	c.c.z = a->c.z;
 	printf("%i %i %i", (int)dx, (int)dy, (int)dz);
-	while (fabs(c.c.x - b->c.x) > 1 || fabs(c.c.y - b->c.y) > 1 || fabs(c.c.z - b->c.z) > 1)	
+	while (fabs(c.c.x - b->c.x) > 1 || fabs(c.c.y - b->c.y) > 1 || \
+	fabs(c.c.z - b->c.z) > 1)
 	{
 		set_pnt_to_img(&c, img);
 		c.c.x += dx;
@@ -49,20 +52,19 @@ static void set_line_to_img(t_point *a, t_point *b, t_img *img)
 	}
 }
 
-void	create_img(t_mlx *displ)
+void		create_img(t_mlx *displ)
 {
 	int	i;
 
 	i = 0;
 	while (i < displ->count_of_points - 1)
 	{
-//		printf("Here %zu %zu \n ", i,displ->pos->nbr_of_pos);
-//		set_pnt_to_img(displ->points + i, &displ->img);
-//		printf("Here %i %i \n", displ->pos->pos[i + 1].x, displ->pos->pos[i + 1].y);
 		if (i + 1 % displ->line_of_points != 0)
-			set_line_to_img(&displ->points[i], &displ->points[i + 1], &displ->img);
+			set_line_to_img(&displ->points[i], &displ->points[i + 1], \
+			&displ->img);
 		if (i + displ->line_of_points < displ->count_of_points)
-			set_line_to_img(&displ->points[i], &displ->points[i + displ->line_of_points], &displ->img);
-		i++;		
-	}		
+			set_line_to_img(&displ->points[i], \
+			&displ->points[i + displ->line_of_points], &displ->img);
+		i++;
+	}
 }
