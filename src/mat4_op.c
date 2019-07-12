640fbbaa42d212ae6e26a669204cf07e38218c20
Michail Pasechnik <michail.goodchild@gmail.com>
2019-07-12 15:17:35 +0300

    mat4_op.c - rename var 2

--- src/mat4_op.c
+++ src/mat4_op.c
@@ -12,13 +12,13 @@
 
 #include "fdf.h"
 
-void	m4_rotate_relative(t_mat4 *m, t_vec3 *p, t_vec3 *rot)
+void	m4_rotate_relative(t_mat4 *m, t_vec3 *p, t_vec3 *r)
 {
 	t_mat4	inv_rel;
 	t_mat4	tmp;
 
 	m4_set_translate(&tmp, p);
 	m4_mul(m, m4_inv(&tmp, &inv_rel), m);
-	m4_rotate(&tmp, rot);
+	m4_rotate(&tmp, r);
 	m4_mul(m, &tmp, m);
 }
